a403e66dd6eda0c14a02138f34f65f1a1c42e33c
azubuike okom <azubuikeokom@gmail.com>
2022-08-12 12:20:10 +0100

    first commit

---
+++ variables.tf
@@ -0,0 +1,8 @@
+variable "access-key"{
+    type = string
+    default = "AKIA3MOQWPOIRIYLPDTS"
+}
+variable "secret-key" {
+    type = string
+    default = "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"
+} 
